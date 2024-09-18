c75e2cc10db43d04f9f4d1ee734424bbad2237c7
KevinJHP19 <huamanpintokevin@fpllefia.com>
2024-09-18 15:30:53 +0000

    primera sesion teoria

--- pac1-primera-app./hola.php
+++ pac1-primera-app./hola.php
@@ -1,11 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+
+    </style>
 </head>
 <body>
+
+
+    <header>
+        
+        <img src="https://drive.google.com/u/0/drive-viewer/AKGpihb7dm0uTkX1iPg8_5rbB5jJdVFrgjGvx2tdKz09PR22swlPbWPWVZe_K_e9u4JNZfSvxjix8Djum6qmZaL21R6p6ig76M8vgT4=s2560" alt="">
+        <h1>Módulo 7 - Práctica 1. Mi primera aplicación en PHP</h1>
+        <ul>
+            <li id="primercolumna"><img src="" alt=""></li>
+            <li>j</li>
+        </ul>
+    </header>
+    <footer>
+        
+    </footer>
+
+
     
 </body>
 </html>
