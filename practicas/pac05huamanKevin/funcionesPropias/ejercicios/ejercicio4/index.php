3ec9c4c4ed133b1df356e1132565fd85047307ec
KevinJHP19 <huamanpintokevin@fpllefia.com>
2024-10-14 16:08:53 +0000

    asd

--- practicas/pac05huamanKevin/funcionesPropias/ejercicios/ejercicio4/index.php
+++ practicas/pac05huamanKevin/funcionesPropias/ejercicios/ejercicio4/index.php
@@ -2,8 +2,20 @@
 
     function convertirTemperatura($temperatura, $escala){
 
-        
-    }
+        if($escala=='F'){
+            $conversion= $temperatura *9/5 + 32;
+            $texto = 'ºF';
+            
+        }else if ($escala=='C'){
+            $conversion= ($temperatura -32)*5/9;
+            $texto = 'ºC';
+        }
+        return($conversion . $texto);
+    };
+
+    echo "La temperatura es " . convertirTemperatura(50, 'C');
+
+    
 
 
 ?>
